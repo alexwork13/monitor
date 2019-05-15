1369a81030ae6e6de3acb4607f2527d30b2df678
sssa13 <alexwork13@ukr.net>
2019-05-15 22:35:27 +0300

    work with cpu and mem parametrs

--- metrics.py
+++ metrics.py
@@ -1,12 +1,43 @@
 import sys
+import psutil
+from collections import namedtuple
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "cpu":
-            print("You see cpu monitor")
+            namedtupl = str(psutil.cpu_times_percent(interval=1, percpu=False))
+            words_list = (namedtupl[namedtupl.find('(')+1:namedtupl.find(')')]).split(',')
+            for i in words_list:
+                print(i)
+             
+
+            
         elif sys.argv[1] == "mem":
-            print("You see mem yest")
+            namedtupl = str(psutil.virtual_memory())
+            words_list = (namedtupl[namedtupl.find('(')+1:namedtupl.find(')')]).split(',')
+            print('Virtual memory:')
+            for i in words_list:
+                print(i)
+
+            
+            namedtupl = str(psutil.swap_memory())
+            words_list = (namedtupl[namedtupl.find('(')+1:namedtupl.find(')')]).split(',')
+            print('---------------')
+            print('Swap memory:')    
+            for i in words_list:
+                print(i)
+           
+           
+
         else:
-            print("Please, enter parametr")
+            print("Please, enter corect parametr: mem or cpu ")
     else:
         print("Please, enter parametr")
